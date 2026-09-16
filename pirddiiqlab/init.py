af189ef9e12b6a3934fce49c4d9a4c252493bcab
Eric Hilary Smith <erichilarysmithsr@gmail.com>
2026-09-16 16:04:56 -0500

    Initialize pirddiiqlab with version and author info

---
+++ pirddiiqlab/init.py
@@ -0,0 +1,11 @@
+"""
+pirddiiqlab: Health data and insights platform for human families
+Version: 0.0.0
+"""
+
+__version__ = "0.0.0"
+__author__ = "make-DEEP-Human-Families-Healthy"
+
+from .backend import create_app
+
+__all__ = ["create_app", "__version__"]
